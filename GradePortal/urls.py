e1ee70256db2c75a8cc874b98c96b8357aae8bf5
Archie Belarmino <110542367+acchiii@users.noreply.github.com>
2026-03-31 13:23:57 +0800

    Fixed for Deployment

--- GradePortal/urls.py
+++ GradePortal/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from portal import views
 from django.conf.urls import handler500, handler404
 
 urlpatterns = [
     path('admin-portal/', admin.site.urls),
     path('', include('portal.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler500 = views.error500
 handler404 = views.error404
