3c160f03675863ba90141f31ce3c2d86c760d5bf
Archie Belarmino <110542367+acchiii@users.noreply.github.com>
2026-03-31 13:01:01 +0800

    Fixed for Deployment

--- GradePortal/settings.py
+++ GradePortal/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -17,7 +17,6 @@ INSTALLED_APPS = [
     'portal',
     'tailwind',
     'theme',
-    'decouple',
 ]
 
 MIDDLEWARE = [
